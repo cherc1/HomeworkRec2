9fc11cd89ca11cdc476addf6ff4a434eb5699bd0
Arina Skripova <skripovaarina1@gmail.com>
2023-12-28 19:18:35 +0300

    3

---
+++ 3/Program.cs
@@ -0,0 +1,15 @@
+﻿int N = 10;
+
+int[] arr = new int[N];
+
+string rec(int[] n)
+{
+    int m = n.Length;
+    if (m == 0)
+    {
+        return "";
+    }
+    return rec(m - 1) + $"{m} ";
+}
+
+Console.WriteLine(rec(N));
