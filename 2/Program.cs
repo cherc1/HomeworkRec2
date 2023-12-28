6fe13d29f8c59ff526d0f4db35678b83b5e8b417
Arina Skripova <skripovaarina1@gmail.com>
2023-12-28 18:28:51 +0300

    2

---
+++ 2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите значения M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значения N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void func(int M, int N)
+{
+    if(M == 0)
+    {
+        return N + 1;
+    }
+    else if(M > 0 && N ==0)
+    {
+        func(M - 1);
+    }
+    else if( M > 0 && N > 0)
+    {
+        func(M - 1(func(N-1)));
+    }
+}
+
+Console.WriteLine(func(m, n));
