dba93c3d7b0053d1e837c463f58edac7ca1f8700
Arina Skripova <skripovaarina1@gmail.com>
2023-12-28 18:10:13 +0300

    1

---
+++ 1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите значения M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значения N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string rec(int n)
+{
+    if (n == m)
+      return "";
+    return rec(n - 1) + $"{n} ";
+}
+
+Console.WriteLine(rec(n));
